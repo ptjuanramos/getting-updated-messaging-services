0ff9d49f523afdb884248204bcd3217ccdd45866
Juan Ramos <ptjuanramos@gmail.com>
2021-06-22 12:40:42 +0100

    Preparing DI for manually registration

--- poc-mediatr/CoordinateBooking/CoordinateBookingHandlers/Handlers/BatchBookingsHandler.cs
+++ poc-mediatr/CoordinateBooking/CoordinateBookingHandlers/Handlers/BatchBookingsHandler.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using CoordinateBookingCommon;
+using CoordinateBookingDataAccess;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CoordinateBooking.Handlers
 {
-    public class BatchBookingsHandler
+    public class BatchBookingsHandler : IRequestHandler<BatchBookingRequest>
     {
+        private readonly IBookingCommand _bookingCommand;
+
+        public BatchBookingsHandler(IBookingCommand bookingCommand)
+        {
+            _bookingCommand = bookingCommand;
+        }
+
+        public Task<Unit> Handle(BatchBookingRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
